9a36c01f479c2d1d1b5d83726b5f777f78198be5
Carlos Alberto Estrela de Souza Junior <69488591+carlosalbertoestrela@users.noreply.github.com>
2022-02-17 17:55:15 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,11 +42,25 @@ function calculo_de_reajuste_salarial(salario){
         if(salario < 280){
             percentual_reajuste = "20%";
             valor_reajuste = salario * .2;
-            novo_salario = salario + valor_reajuste;
         }
+        else if (salario < 700){
+            percentual_reajuste = "15%";
+            valor_reajuste = salario * .15;
+        }
+        else if (salario < 1500){
+            percentual_reajuste = "10%";
+            valor_reajuste = salario * .1;
+        }
+        else{
+            percentual_reajuste = "5%";
+            valor_reajuste = salario * .05;
+        }
+        novo_salario = salario + valor_reajuste;
 
+        return [salario, percentual_reajuste, valor_reajuste, novo_salario];
     }
-    return [salario, percentual_reajuste, valor_reajuste, novo_salario];
+    
+    
 }
 
 // Atividade 3:
@@ -72,7 +86,7 @@ function folha_de_pagamento(salario_bruto){
         
         let fgts = salario_bruto * .11;
         let sindicato = salario_bruto * .03;
-        let porcentagem_imposto_renda = (salario_bruto) => {
+        let porcentagem_ir = (salario_bruto) => {
             if(salario_bruto > 2500){
                 return .2;
             }
@@ -84,25 +98,66 @@ function folha_de_pagamento(salario_bruto){
             }
             else{
                 return 0;
-            }
-            
+            } 
         }
     
-        let imposto_renda = salario_bruto  * porcentagem_imposto_renda(salario_bruto);
-        let total_descontos = imposto_renda + sindicato + imposto_renda;
+        let ir = salario_bruto  * porcentagem_ir(salario_bruto);
+        let total_descontos = ir + sindicato + ir;
         let salario_liquido = salario_bruto - (total_descontos)
 
         return {
-            "salario Bruto": salario_bruto,
-            "imposto de Renda": imposto_renda,
+            "Salario Bruto": salario_bruto,
+            "Imposto de Renda": ir,
             "FGTS": fgts,
             "Sindicato": sindicato,
-            "salarioLiquido": salario_liquido,
-            "Porcentagem imposto de renda": (porcentagem_imposto_renda(salario_bruto)*100 + "%")
+            "SalarioLiquido": salario_liquido,
+            "Porcentagem imposto de renda": (porcentagem_ir(salario_bruto)*100 + "%")
         }
     }
     
 
 }
 
-console.log(folha_de_pagamento(3000))
+// Interação com o DOM
+
+// at1
+function q1(){
+    let at1 = document.querySelector(".at1_answer");
+    let nota1 = parseFloat(document.querySelector("#nota1").value);
+    let nota2 = parseFloat(document.querySelector("#nota2").value);
+
+    at1.innerHTML = `<hr><strong>${calcula_media(nota1,nota2)}</strong><hr>`;
+}
+// at2
+function q2(){
+    let at2 = document.querySelector(".at2_answer");
+    let salario =parseFloat(document.querySelector(".salario").value) ;
+    let info = calculo_de_reajuste_salarial(salario);
+    at2.innerHTML = 
+    `<hr> <strong>
+    Antigo salário: R$${info[0]} <br>
+    Percential do aumento: ${info[1]} <br>
+    Valor do aumento: R$${info[2].toFixed(2)} <br>
+    Novo salário: R$${info[3].toFixed(2)} <br>
+    </strong> <hr>`
+    ;
+}
+// at3
+function q3(){
+    let at3 = document.querySelector(".at3_answer");
+    let hora_trabalhada = parseInt(document.querySelector(".hora_trabalhada").value);
+    let valor_hora = parseFloat(document.querySelector(".valor_hora").value);
+    
+
+    salario_bruto = calcula_salario_bruto(valor_hora,hora_trabalhada);
+    folha = folha_de_pagamento(salario_bruto)    
+
+    at3.innerHTML = `
+        ${folha["Salario Bruto"]}
+        
+        
+    `
+
+}
+
+ 
